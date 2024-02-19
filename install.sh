4047ee546d5b4be1a359dbe44b44a9335fd7b73d
aflaag <alessio.bandiera02@gmail.com>
2024-02-19 15:28:32 +0100

    added nodejs to install.sh

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ pakepkg -si
 #   - kitty
 #   - nitrogen
 #   - Neovim
+#   - nodejs
 #   - PulseAudio Volume Control
 #   - PipeWire
 #   - Polybar
@@ -63,7 +64,7 @@ pakepkg -si
 #   - xcolor
 #   - xpad
 #   - zathura (and dependencies)
-paru -S archlinux-keyring ark bat betterlockscreen bluez bluez-utils bspwm btop code droidcam dunst firefox gimp gnome-keyring htop icu69-bin jre-openjdk kitty nitrogen neovim pavucontrol pipewire-alsa pipewire-cli pipewire-media-session pipewire-pulse polybar python python-aiohttp python-pillow python-pip python-wheel ripgrep ristretto rofi rustup sddm spectacle speedcrunch sxhkd telegram-desktop texlive-basic texlive-bibtexextra texlive-fontsrecommended texlive-fontsextra texlive-latex texlive-langitalian texlive-mathscience texlive-meta thunar timeshift unrar unzip v4l2loopback-dkms vlc xclip xcolor xpad zathura zathura-pdf-mupdf
+paru -S archlinux-keyring ark bat betterlockscreen bluez bluez-utils bspwm btop code droidcam dunst firefox gimp gnome-keyring htop icu69-bin jre-openjdk kitty nitrogen neovim nodejs pavucontrol pipewire-alsa pipewire-cli pipewire-media-session pipewire-pulse polybar python python-aiohttp python-pillow python-pip python-wheel ripgrep ristretto rofi rustup sddm spectacle speedcrunch sxhkd telegram-desktop texlive-basic texlive-bibtexextra texlive-fontsrecommended texlive-fontsextra texlive-latex texlive-langitalian texlive-mathscience texlive-meta thunar timeshift unrar unzip v4l2loopback-dkms vlc xclip xcolor xpad zathura zathura-pdf-mupdf
 
 # install for:
 #   - Oh My Zsh
