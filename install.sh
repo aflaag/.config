c0e9518d2421560c86777b02d56b35852d4bdc3a
aflaag <alessio.bandiera02@gmail.com>
2025-07-19 09:55:51 +0200

    install.sh update

--- install.sh
+++ install.sh
@@ -79,12 +79,12 @@ pakepkg -si
 #   - unzip
 #   - Visual Studio Code
 #   - VLC
+#   - Xbox drivers
 #   - xclip
 #   - xcolor
-#   - xpad
 #   - zathura (and dependencies)
 #   - zoxide
-paru -S archlinux-keyring ark bat betterlockscreen bluez bluez-utils bspwm btop code droidcam dunst dust firefox gimp github-cli gnome-keyring htop icu69-bin jre-openjdk kitty nitrogen neovim nodejs pavucontrol pipewire-alsa pipewire-cli pipewire-media-session pipewire-pulse polybar python python-aiohttp python-pillow python-pip python-wheel ripgrep ristretto rofi rustup sddm spectacle speedcrunch sxhkd telegram-desktop texlive-basic texlive-bibtexextra texlive-fontsrecommended texlive-fontsextra texlive-latex texlive-langitalian texlive-mathscience texlive-meta thunar timeshift tldr unrar unzip v4l2loopback-dkms vlc xclip xcolor xpad zathura zathura-pdf-mupdf zoxide
+paru -S archlinux-keyring ark bat betterlockscreen bluez bluez-utils bspwm btop code droidcam dunst dust firefox gimp github-cli gnome-keyring htop icu69-bin jre-openjdk kitty nitrogen neovim nodejs pavucontrol pipewire-alsa pipewire-cli pipewire-media-session pipewire-pulse polybar python python-pip python-wheel ripgrep ristretto rofi rustup sddm spectacle speedcrunch sxhkd telegram-desktop texlive-basic texlive-bibtexextra texlive-fontsrecommended texlive-fontsextra texlive-latex texlive-langitalian texlive-mathscience texlive-meta thunar timeshift tldr unrar unzip v4l2loopback-dkms vlc xboxdrv xclip xcolor xpad zathura zathura-pdf-mupdf zoxide
 
 ########## SYSTEMCTL ##########
 
